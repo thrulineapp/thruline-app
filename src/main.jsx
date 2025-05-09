551dea88332fcd790d4bd6a49c0dceb224450c63
thrulineapp <alrapallo@gmail.com>
2025-05-09 09:07:04 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
